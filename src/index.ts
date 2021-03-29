bcfe4380a3ac00dbbbb02b98d31a67018ec3fb2a
mrLuisFer <lolesuncrak@gmail.com>
2021-03-29 16:14:25 -0600

    fix: corregi algunos detalles del client
    feat: agregue un maximo de listeners

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { Client } from "discord.js"
 import { config } from "./config/config"
 
+// Functions
+import { ping } from "./functions/ping"
+
 const client: Client = new Client({
   partials: ["MESSAGE", "REACTION"],
 })
 
-// Functions
-import { ping } from "./functions/ping"
+client.on("ready", () => {
+  if (client === null) return
+  else {
+    console.log(`Bot is ready as ${client?.user?.username}`)
+    client?.user?.setStatus("online")
+    // handles the maximum available event or command listener
+    client.setMaxListeners(20)
+  }
+})
 
 client.on("message", (message) => {
   // Validate to the messages isn't from the bot
